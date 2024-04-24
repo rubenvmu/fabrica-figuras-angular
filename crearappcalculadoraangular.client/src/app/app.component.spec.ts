66eae5ee99d09e6d297aec2b97d3ccab1a1769c3
rvmu <rvmu@rvmu.es>
2024-04-24 14:56:14 +0200

    vol.4

--- crearappcalculadoraangular.client/src/app/app.component.spec.ts
+++ crearappcalculadoraangular.client/src/app/app.component.spec.ts
@@ -1,3 +1,3 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ComponenteCalculadora } from './app.component';
+import { AppComponent } from './app.component';
